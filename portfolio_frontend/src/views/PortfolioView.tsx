50c6ef2de2d2821a73dc0c8069f25efc365de11b
Adornado B Cabalbag Jr. <acabalbag22@gmail.com>
2026-04-01 00:40:48 +0800

    Modified Move Smart Contract Portfolio

--- portfolio_frontend/src/views/PortfolioView.tsx
+++ portfolio_frontend/src/views/PortfolioView.tsx
@@ -98,7 +98,7 @@ const PortfolioView = () => {
   // ==========================================================================
   // STATE MANAGEMENT
   // ==========================================================================
-  const objectId = "0xa3343391df96e28464499f4c209d51bf209c07392fdeea97bfeee59e7550f020";
+  const objectId = "0xdef8b59daf7bb62d6620da882f232c311d7e2f383db54113c51d5870ade65464";
   
   // Network state - default to testnet, can be changed if needed
   const [currentNetwork, setCurrentNetwork] = useState<"testnet" | "mainnet">("mainnet");
